d93de8ece47cd53ef94fd0e204f33f3ea0881283
J-J-Cooper <134175036+J-J-Cooper@users.noreply.github.com>
2023-05-21 00:27:43 +0100

    Initial files

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html lang="en-uk">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Cooper</title>
+		<meta name="description" content="Josh Cooper's personal website.">
+		<meta name="author" content="Josh Cooper">
+
+		<meta property="og:title" content="Josh Cooper">
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="Makadee.com">
+		<meta property="og:description" content="Josh Cooper's personal website.">
+		<meta property="og:image" content="image.png">
+
+		<link rel="icon" href="/favicon.ico">
+		<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+		<link rel="stylesheet" href="resources/css/navbar-style-light.css">
+		<link rel="stylesheet" href="resources/css/light-style.css">
+
+	</head>
+
+	<body>
+		<!-- php header-->
+		<?php
+			include ('resources/navbar.php')
+		?>
+		
+		<div id="main-content">
+			<h2 class="main-content__heading">
+				This is a heading.
+			</h2>
+			<p class="main-content__text">
+				Lorem ipsum dolor sit amet...
+				The standard Lorem Ipsum passage, used since the 1500s
+				<br>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				<br>
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				<br>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+				<br>
+				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				<br>
+				<br>
+				Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+				<br>
+				"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+				<br>
+				Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+				<br>
+				Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+				<br>
+				Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+				<br>
+				Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+				
+				<br>
+				<br>
+				<br>
+				
+				Lorem ipsum dolor sit amet...
+				The standard Lorem Ipsum passage, used since the 1500s
+				<br>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				<br>
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				<br>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+				<br>
+				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				<br>
+				<br>
+				Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+				<br>
+				"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+				<br>
+				Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+				<br>
+				Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+				<br>
+				Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+				<br>
+				Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+			</p>
+		</div>
+		<!-- PHP footer -->
+		<!-- your content here... -->
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
---
+++ resources/navbar.php
@@ -0,0 +1,47 @@
+<div id="nav">
+	<h1 id="nav__heading">
+		<a id="nav__link" href="website-home.html">
+			JC
+		</a>
+	</h1>
+	<br>
+	<div id="nav__container">
+		<a href="website-home.html">
+			<div class="nav__button">
+				Home
+			</div>
+		</a>
+		<a href="website-cv.html">
+			<div class="nav__button">
+				CV
+			</div>
+		</a>
+		<a href="website-hindi.html">
+			<div class="nav__button nav__button--current">
+				Hindi
+			</div>
+		</a>
+		
+		<!-- Navigation dropdown -->
+		<div class="nav__dropdown">
+			Hindi </a>
+			
+				<a href="VOWELS PAGE GOES HERE"> Vowels </a>
+				<a href="CONSONANTS PAGE GOES HERE"> Consonants </a>
+				<a href="NUMBERS PAGE GOES HERE"> Numbers </a>
+			
+		</div>
+		
+		
+		<a href="website-hindi.html">
+			<div class="nav__button">
+				Floorplan project
+			</div>
+		</a>
+		<a href="website-hindi.html">
+			<div class="nav__button">
+				Contact me
+			</div>
+		</a>
+	</div>
+</div>
